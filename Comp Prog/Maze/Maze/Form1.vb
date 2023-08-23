98a58d89db6457fe205e08c64d1b1b244195364f
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-08-23 12:19:15 -0400

    real

--- Comp Prog/Maze/Maze/Form1.vb
+++ Comp Prog/Maze/Maze/Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
+        MsgBox("Dominick")
     End Sub
 End Class
