4174df0864c00ac0a02eafcc8d316523a768d819
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-08-24 12:21:10 -0400

    L

--- Comp Prog/Maze/Maze/Form1.Designer.vb
+++ Comp Prog/Maze/Maze/Form1.Designer.vb
@@ -332,6 +332,9 @@ Partial Class Form1
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
