05be1d374f83f8df8baa50c0bb4489f16bbbadca
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-09-07 12:20:40 -0400

    mario

---
+++ Comp Prog/Mario/Mario/Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.mario = New System.Windows.Forms.PictureBox()
+        CType(Me.mario, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'mario
+        '
+        Me.mario.Image = CType(resources.GetObject("mario.Image"), System.Drawing.Image)
+        Me.mario.Location = New System.Drawing.Point(100, 100)
+        Me.mario.Name = "mario"
+        Me.mario.Size = New System.Drawing.Size(40, 64)
+        Me.mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.mario.TabIndex = 0
+        Me.mario.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(500, 500)
+        Me.Controls.Add(Me.mario)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.mario, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents mario As PictureBox
+End Class
---
+++ Comp Prog/Mario/Mario/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.W And mario.Location.Y > 0 Then
+            mario.Location = New Point(mario.Location.X, mario.Location.Y - 4)
+            mario.Image = Image.FromFile("up1.png")
+        End If
+        If e.KeyCode = Keys.A And mario.Location.X > 0 Then
+            mario.Location = New Point(mario.Location.X - 4, mario.Location.Y)
+            mario.Image = Image.FromFile("left1.png")
+        End If
+        If e.KeyCode = Keys.S And mario.Location.Y < 436 Then
+            mario.Location = New Point(mario.Location.X, mario.Location.Y + 4)
+            mario.Image = Image.FromFile("down1.png")
+        End If
+        If e.KeyCode = Keys.D And mario.Location.X < 460 Then
+            mario.Location = New Point(mario.Location.X + 4, mario.Location.Y)
+            mario.Image = Image.FromFile("right1.png")
+        End If
+        If e.KeyCode = Keys.Q And mario.Location.X > 0 And mario.Location.Y > 0 Then
+            mario.Location = New Point(mario.Location.X - 4, mario.Location.Y - 4)
+        End If
+        If e.KeyCode = Keys.E And mario.Location.X < 460 And mario.Location.Y < 0 Then
+            mario.Location = New Point(mario.Location.X + 4, mario.Location.Y - 4)
+        End If
+        If e.KeyCode = Keys.W And Keys.A And mario.Location.X > 0 And mario.Location.Y > 0 Then
+            mario.Location = New Point(mario.Location.X - 4, mario.Location.Y - 4)
+        End If
+    End Sub
+End Class
