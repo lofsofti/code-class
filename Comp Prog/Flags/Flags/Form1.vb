43475f713250086cd38366948a82b6c740a28b3f
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-09-18 12:17:24 -0400

    goofy ahh

--- Comp Prog/Flags/Flags/Form1.vb
+++ Comp Prog/Flags/Flags/Form1.vb
@@ -7,6 +7,8 @@
         Dim blackPen As New Pen(Color.Black, 2) 'Color, Line Width
         Dim redPen As New Pen(Color.Red, 1) 'Red Pen, 1 pixel wide
         Dim bluePen As New Pen(Color.Blue, 5) 'Blue Pen, 5 pixels wide
+        Dim BLACK As New Pen(Color.Black, 7)
+        Dim blacke As New Pen(Color.Black, 8)
 
         'French
         g.FillRectangle(Brushes.Blue, 0, 0, 100, 200)
@@ -40,10 +42,33 @@
         g.FillPie(Brushes.White, 700, 245, 100, 100, 270, 180)
         g.FillPie(Brushes.Black, 700, 245, 100, 100, 90, 180)
         g.DrawEllipse(blackPen, 700, 245, 100, 100)
-        g.FillEllipse(Brushes.White, 726, 246, 50, 51)
-        g.FillEllipse(Brushes.Black, 726, 293, 50, 51)
+        g.FillEllipse(Brushes.White, 725, 245, 50, 50)
+        g.FillEllipse(Brushes.Black, 725, 295, 50, 50)
         g.FillEllipse(Brushes.Black, 740, 263, 17, 17)
         g.FillEllipse(Brushes.White, 741, 309, 17, 17)
+
+        'Custom
+        g.FillRectangle(Brushes.Green, 0, 400, 90, 90)
+        g.FillRectangle(Brushes.Yellow, 0, 510, 90, 90)
+        g.FillRectangle(Brushes.Yellow, 110, 400, 190, 90)
+        g.FillRectangle(Brushes.Green, 110, 510, 190, 90)
+        g.FillPie(Brushes.Yellow, 38, 438, 124, 124, 180, 90)
+        g.FillPie(Brushes.Green, 38, 438, 124, 124, 270, 90)
+        g.FillPie(Brushes.Yellow, 38, 438, 124, 124, 0, 90)
+        g.FillPie(Brushes.Green, 38, 438, 124, 124, 90, 90)
+        g.FillRectangle(Brushes.Black, 0, 490, 300, 20)
+        g.FillRectangle(Brushes.Black, 90, 400, 20, 200)
+        g.DrawEllipse(BLACK, 38, 438, 124, 124)
+        g.FillEllipse(Brushes.Black, 50, 450, 100, 100)
+        g.FillRectangle(Brushes.Yellow, 90, 470, 20, 60)
+        g.FillRectangle(Brushes.Yellow, 70, 490, 60, 20)
+
+        'Peace
+        g.FillRectangle(Brushes.Green, 300, 400, 300, 67)
+        g.FillRectangle(Brushes.Yellow, 300, 467, 300, 67)
+        g.FillRectangle(Brushes.Red, 300, 533, 300, 67)
+        g.FillEllipse(Brushes.White, 390, 440, 125, 125)
+        g.DrawEllipse(blacke, 390, 440, 125, 125)
 
         'Flag Boxes
         g.DrawLine(blackPen, 0, 200, 900, 200)
