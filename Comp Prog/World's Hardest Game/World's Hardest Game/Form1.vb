1b8c50e1948882c5b1c8df9e223f72ac27cdbdda
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-09-25 12:09:15 -0400

    hard game

--- Comp Prog/World's Hardest Game/World's Hardest Game/Form1.vb
+++ Comp Prog/World's Hardest Game/World's Hardest Game/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Dim boxDir As String
     Dim ballDir As String = "left"
+    Dim deathCount As Integer = 0
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         If e.KeyCode = Keys.W Then boxDir = "up"
         If e.KeyCode = Keys.A Then boxDir = "left"
@@ -12,6 +13,11 @@
         moveBox()
         checkBallCollisions()
         checkWallCollisions()
+        If Box.Location.X > 340 Then
+            boxTimer.Enabled = False
+            ballTimer.Stop()
+            MsgBox("You cleared Level 1!")
+        End If
     End Sub
 
     Public Sub moveBox()
@@ -43,22 +49,67 @@
 
     Public Sub checkBallCollisions()
         If Box.Bounds.IntersectsWith(Ball1.Bounds) Then
+            deathCount = deathCount + 1
+            DeathsLabel.Text = "Deaths:" & deathCount
             Box.Location = New Point(62, 172)
         End If
         If Box.Bounds.IntersectsWith(Ball2.Bounds) Then
+            deathCount = deathCount + 1
+            DeathsLabel.Text = "Deaths:" & deathCount
             Box.Location = New Point(62, 172)
         End If
         If Box.Bounds.IntersectsWith(Ball3.Bounds) Then
+            deathCount = deathCount + 1
+            DeathsLabel.Text = "Deaths:" & deathCount
             Box.Location = New Point(62, 172)
         End If
         If Box.Bounds.IntersectsWith(Ball4.Bounds) Then
+            deathCount = deathCount + 1
+            DeathsLabel.Text = "Deaths:" & deathCount
             Box.Location = New Point(62, 172)
         End If
     End Sub
 
-    Public Sub checkWallCollision()
+    Public Sub checkWallCollisions()
         If Box.Bounds.IntersectsWith(Wall1.Bounds) Then
-
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall2.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall3.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall4.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall5.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall6.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall7.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall8.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall9.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall10.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall11.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall12.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall13.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall14.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall15.Bounds) Then
+            pushBoxBack()
+        ElseIf Box.Bounds.IntersectsWith(Wall16.Bounds) Then
+            pushBoxBack()
         End If
+    End Sub
+
+    Public Sub pushBoxBack()
+        If boxDir = "up" Then Box.Location = New Point(Box.Location.X, Box.Location.Y + 5)
+        If boxDir = "down" Then Box.Location = New Point(Box.Location.X, Box.Location.Y - 5)
+        If boxDir = "left" Then Box.Location = New Point(Box.Location.X + 5, Box.Location.Y)
+        If boxDir = "right" Then Box.Location = New Point(Box.Location.X - 5, Box.Location.Y)
     End Sub
 End Class
