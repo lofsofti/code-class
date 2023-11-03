34ff4ffc3b3bf5b5131518970b14ac8df678a913
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-11-03 12:07:11 -0400

    Epic label plus pictures

--- Comp Prog/ConnectFour/ConnectFour/Form1.vb
+++ Comp Prog/ConnectFour/ConnectFour/Form1.vb
@@ -3,10 +3,37 @@
     Dim r, c As Integer
     Dim player As String = "red"
     Dim gameOver As Boolean = False
+    Dim connect4, turn As Label
+    Dim playerTurn As PictureBox
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         buildBoard()
     End Sub
     Public Sub buildBoard()
+        connect4 = New Label
+        connect4.Size = New Size(75, 15)
+        connect4.Location = New Point(185, 10)
+        connect4.TextAlign = ContentAlignment.MiddleCenter
+        connect4.AutoSize = False
+        connect4.Text = "Connect Four"
+        Controls.Add(connect4)
+        turn = New Label
+        turn.Size = New Size(30, 15)
+        turn.Location = New Point(300, 15)
+        turn.TextAlign = ContentAlignment.MiddleCenter
+        turn.AutoSize = False
+        turn.Text = "Turn:"
+        Controls.Add(turn)
+        'fix updating color
+        playerTurn = New PictureBox
+        If player = "red" Then
+            playerTurn.Image = Image.FromFile("red.png")
+        Else
+            playerTurn.Image = Image.FromFile("yellow.png")
+        End If
+        playerTurn.SizeMode = PictureBoxSizeMode.StretchImage
+        playerTurn.Size = New Size(50, 50)
+        playerTurn.Location = New Point(350, 0)
+        Controls.Add(playerTurn)
         For c = 1 To 7
             For r = 1 To 6
                 board(c, r) = New PictureBox
@@ -48,22 +75,34 @@
     Public Sub checkWin()
         For r = 1 To 6
             For c = 1 To 4
-                If board(c, r).Tag = player And board(c + 1, r).Tag = player And board(c + 2, r).Tag = player And board(c + 3, r).Tag = player Then MsgBox(player & " wins!!")
+                If board(c, r).Tag = player And board(c + 1, r).Tag = player And board(c + 2, r).Tag = player And board(c + 3, r).Tag = player Then
+                    MsgBox(player & " wins!!")
+                    gameOver = True
+                End If
             Next
         Next
         For r = 1 To 3
             For c = 1 To 4
-                If board(c, r).Tag = player And board(c, r + 1).Tag = player And board(c, r + 2).Tag = player And board(c, r + 3).Tag = player Then MsgBox(player & " wins!!")
+                If board(c, r).Tag = player And board(c, r + 1).Tag = player And board(c, r + 2).Tag = player And board(c, r + 3).Tag = player Then
+                    MsgBox(player & " wins!!")
+                    gameOver = True
+                End If
             Next
         Next
         For r = 1 To 3
             For c = 1 To 4
-                If board(c, r).Tag = player And board(c + 1, r + 1).Tag = player And board(c + 2, r + 2).Tag = player And board(c + 3, r + 3).Tag = player Then MsgBox(player & " wins!!")
+                If board(c, r).Tag = player And board(c + 1, r + 1).Tag = player And board(c + 2, r + 2).Tag = player And board(c + 3, r + 3).Tag = player Then
+                    MsgBox(player & " wins!!")
+                    gameOver = True
+                End If
             Next
         Next
         For r = 4 To 6
             For c = 1 To 4
-                If board(c, r).Tag = player And board(c + 1, r - 1).Tag = player And board(c + 2, r - 2).Tag = player And board(c + 3, r - 3).Tag = player Then MsgBox(player & " wins!!")
+                If board(c, r).Tag = player And board(c + 1, r - 1).Tag = player And board(c + 2, r - 2).Tag = player And board(c + 3, r - 3).Tag = player Then
+                    MsgBox(player & " wins!!")
+                    gameOver = True
+                End If
             Next
         Next
     End Sub
