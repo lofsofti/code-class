30d527e30a9ab3ac7e56340babeffac6c2150840
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-11-06 12:19:54 -0500

    organizing + array(lists)

---
+++ Comp Prog/Assignments/Simon/Simon/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Simon")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Simon")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("259a2c48-84b7-483e-a613-7a1b45e6b7c7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Comp Prog/Demos/ListDemo/ListDemo/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Dim nums As New List(Of Integer)
+    Dim n, r As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For n = 1 To 20
+            nums.Add(n)
+        Next
+        'MsgBox(nums.Count)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For n = 0 To nums.Count - 1
+            ListBox1.Items.Add(nums.Item(n))
+        Next
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For n = nums.Count - 1 To 0 Step -1
+            ListBox1.Items.Add(nums.Item(n))
+        Next
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Randomize()
+        While nums.Count > 0
+            r = Int(Rnd() * nums.Count)
+            ListBox1.Items.Add(nums.Item(r))
+            nums.RemoveAt(r)
+        End While
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        nums.Clear()
+        ListBox1.Items.Clear()
+    End Sub
+End Class
