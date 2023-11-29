23714d207e03e6c58333d4ad68e4c2060a2ee7d6
lofsofti <126016644+lofsofti@users.noreply.github.com>
2023-11-29 12:18:48 -0500

    button + menu

--- Comp Prog/final project/grub clicker/grub clicker/Form1.Designer.vb
+++ Comp Prog/final project/grub clicker/grub clicker/Form1.Designer.vb
@@ -22,31 +22,51 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblStartbtn = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'lblStartbtn
         '
-        Me.Button1.Location = New System.Drawing.Point(344, 195)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.lblStartbtn.BackColor = System.Drawing.Color.Transparent
+        Me.lblStartbtn.Font = New System.Drawing.Font("Malgun Gothic", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStartbtn.Location = New System.Drawing.Point(337, 293)
+        Me.lblStartbtn.Name = "lblStartbtn"
+        Me.lblStartbtn.Size = New System.Drawing.Size(160, 49)
+        Me.lblStartbtn.TabIndex = 1
+        Me.lblStartbtn.Text = ">    Start    <"
+        Me.lblStartbtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(108, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(598, 278)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblStartbtn)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents Button1 As Button
+    Friend WithEvents lblStartbtn As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Comp Prog/final project/grub clicker/grub clicker/Form1.vb
+++ Comp Prog/final project/grub clicker/grub clicker/Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub lblStartbtn_Click(sender As Object, e As EventArgs) Handles lblStartbtn.Click
         Me.Hide()
         Form2.ShowDialog()
     End Sub
